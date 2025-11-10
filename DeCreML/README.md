6f82b877093ebc34a23b8ab556aa8823b35809fe
enrique1497m-dot <enrique1497m@gmail.com>
2025-11-10 15:42:05 -0600

    Add files via upload

---
+++ DeCreML/README.md
@@ -0,0 +1,1 @@
+DeCreML README placeholder
---
+++ DeCreML/processing/provider.py
@@ -0,0 +1,10 @@
+
+from qgis.core import QgsProcessingProvider
+from .decreml_algorithm import Decreml120
+
+class DeCreMLProvider(QgsProcessingProvider):
+    def loadAlgorithms(self):
+        self.addAlgorithm(Decreml120())
+    def id(self): return "DeCreML"
+    def name(self): return "DeCreML"
+    def longName(self): return "Detección de Crecimientos ML"
